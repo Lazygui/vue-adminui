07ecd62bbe105dde71cabdb5ab0e5147022912ba
zhouzhijun <15642532452@163.com>
2025-04-29 10:17:33 +0800

    style(Login.vue): 格式化代码以提高可读性
    对Login.vue文件进行了代码格式化，包括缩进、换行和引号的一致性调整，以提升代码的可读性和维护性。

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,193 +1,207 @@
 <script setup lang="ts">
-import ZCard from '@/components/library/ZCard.vue'
-import ZInput from '@/components/library/ZInput.vue'
-import ZCheckbox from '@/components/library/ZCheckbox.vue'
-import ZButton from '@/components/library/ZButton.vue'
-import { UserIcon, LockIcon } from '@/components/svg'
-import ThemeButton from "@admin/login/ThemeButton.vue"
-import { useLoading } from "@/hooks/private/usePlugin"
+import ZCard from "@/components/library/ZCard.vue";
+import ZInput from "@/components/library/ZInput.vue";
+import ZCheckbox from "@/components/library/ZCheckbox.vue";
+import ZButton from "@/components/library/ZButton.vue";
+import { UserIcon, LockIcon } from "@/components/svg";
+import ThemeButton from "@admin/login/ThemeButton.vue";
+import { useLoading } from "@/hooks/private/usePlugin";
 import { isExpire, diffSystem } from "@/hooks/useUtils";
-useLoading.show()
+useLoading.show();
 interface Config {
-  system: string
+       system: string;
 }
 interface IForm {
-  username: string
-  password: string
+       username: string;
+       password: string;
 }
-const router = useRouter()
-const loading = ref<boolean>(false)
+const router = useRouter();
+const loading = ref<boolean>(false);
 const form = ref<IForm>({
-  username: '',
-  password: '',
-})
+       username: "",
+       password: ""
+});
 const config = ref<Config>({
-  system: ''
-})
-const remember = ref<boolean>(false)
-const disabled = ref<boolean>(false)
+       system: ""
+});
+const remember = ref<boolean>(false);
+const disabled = ref<boolean>(false);
 const submit = () => {
-  disabled.value = true
-  //本地保存密码
-  if (remember.value) {
-    storage.setItem(StorageKeys.USERNAME, form.value.username)
-    storage.setItem(StorageKeys.PASSWORD, form.value.password)
-  }
-  router.push(`/${config.value.system}`)
-  disabled.value = false
-}
+       disabled.value = true;
+       //本地保存密码
+       if (remember.value) {
+              storage.setItem(StorageKeys.USERNAME, form.value.username);
+              storage.setItem(StorageKeys.PASSWORD, form.value.password);
+       }
+       router.push(`/${config.value.system}`);
+       disabled.value = false;
+};
 onMounted(async () => {
-  const system = await diffSystem()
-  if (!isExpire()) {
-    useLoading.hidden()
-    router.push(`/${system}`)
-    return
-  }
-  form.value.password = ''
-  form.value.username = ''
-  loading.value = true
-  useLoading.hidden()
-  config.value.system = system
-})
+       const system = await diffSystem();
+       if (!isExpire()) {
+              useLoading.hidden();
+              router.push(`/${system}`);
+              return;
+       }
+       form.value.password = "";
+       form.value.username = "";
+       loading.value = true;
+       useLoading.hidden();
+       config.value.system = system;
+});
 </script>
 
 <template>
-  <div class="login flex items-center justify-center to-indigo-900 from-blue-900\/70" v-if="loading">
-    <z-card class="login-card">
-      <div class="card-body">
-        <div class="text-center">
-          <h2 class="h2 text-base-content">用户登录</h2>
-          <p class="tips text-base-content">请输入您的账号和密码</p>
-        </div>
-        <div class="form">
-          <form @submit.prevent="submit">
-            <z-input required class="form-input" label="账号" :icon="UserIcon" v-model="form.username"
-              placeholder="请输入账号"></z-input>
-            <z-input required label="密码" :icon="LockIcon" type="password" v-model="form.password"
-              placeholder="请输入密码"></z-input>
-            <div class="form-config flex justify-between items-center">
-              <div class="flex items-center">
-                <z-checkbox label="记住密码" type="checkbox" v-model="remember"></z-checkbox>
-              </div>
-              <ThemeButton></ThemeButton>
-            </div>
-            <div class="text-center">
-              <z-button :disabled="disabled" type="primary" btn-type="submit">{{ disabled ? '登录中...' : '登录'
-              }}</z-button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </z-card>
-  </div>
+       <div class="login flex items-center justify-center to-indigo-900 from-blue-900\/70" v-if="loading">
+              <z-card class="login-card">
+                     <div class="card-body">
+                            <div class="text-center">
+                                   <h2 class="h2 text-base-content">用户登录</h2>
+                                   <p class="tips text-base-content">请输入您的账号和密码</p>
+                            </div>
+                            <div class="form">
+                                   <form @submit.prevent="submit">
+                                          <z-input
+                                                 required
+                                                 class="form-input"
+                                                 label="账号"
+                                                 :icon="UserIcon"
+                                                 v-model="form.username"
+                                                 placeholder="请输入账号"
+                                          ></z-input>
+                                          <z-input
+                                                 required
+                                                 label="密码"
+                                                 :icon="LockIcon"
+                                                 type="password"
+                                                 v-model="form.password"
+                                                 placeholder="请输入密码"
+                                          ></z-input>
+                                          <div class="form-config flex justify-between items-center">
+                                                 <div class="flex items-center">
+                                                        <z-checkbox label="记住密码" type="checkbox" v-model="remember"></z-checkbox>
+                                                 </div>
+                                                 <ThemeButton></ThemeButton>
+                                          </div>
+                                          <div class="text-center">
+                                                 <z-button :disabled="disabled" type="primary" btn-type="submit">{{
+                                                        disabled ? "登录中..." : "登录"
+                                                 }}</z-button>
+                                          </div>
+                                   </form>
+                            </div>
+                     </div>
+              </z-card>
+       </div>
 </template>
 
 <style scoped lang="scss">
 .login {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  background-image: linear-gradient(to right, rgba(28, 57, 142, 0.7), transparent), url('/background.png');
-  background-size: cover;
-  background-repeat: no-repeat;
-  inset: 0;
-  // background-image: linear-gradient(to right in oklab);
+       position: relative;
+       width: 100%;
+       height: 100%;
+       display: flex;
+       background-image: linear-gradient(to right, rgba(28, 57, 142, 0.7), transparent), url("/background.png");
+       background-size: cover;
+       background-repeat: no-repeat;
+       inset: 0;
+       // background-image: linear-gradient(to right in oklab);
 
+       .login-card {
+              width: 24rem;
 
+              .card-body {
+                     width: 100%;
 
-  .login-card {
-    width: 24rem;
+                     .form {
+                            width: 100%;
+                            margin-top: 2rem;
+                            box-sizing: border-box;
 
-    .card-body {
-      width: 100%;
+                            .form-input {
+                                   width: 100%;
+                            }
 
-      .form {
-        width: 100%;
-        margin-top: 2rem;
-        box-sizing: border-box;
+                            .form-config {
+                                   margin-block-start: 0;
+                                   margin-block-end: 1.5rem;
+                            }
+                     }
 
-        .form-input {
-          width: 100%;
-        }
+                     .h2 {
+                            margin-top: 0;
+                            font-size: 1.875rem;
+                            line-height: calc(2.25 / 1.875);
+                            letter-spacing: -0.025rem;
+                            font-weight: 800;
+                            margin-bottom: 0.25rem;
+                     }
 
-        .form-config {
-          margin-block-start: 0;
-          margin-block-end: 1.5rem;
-        }
-      }
-
-      .h2 {
-        margin-top: 0;
-        font-size: 1.875rem;
-        line-height: calc(2.25 / 1.875);
-        letter-spacing: -0.025rem;
-        font-weight: 800;
-        margin-bottom: 0.25rem;
-      }
-
-      .tips {
-        font-size: 0.875rem;
-      }
-    }
-  }
+                     .tips {
+                            font-size: 0.875rem;
+                     }
+              }
+       }
 }
 
 .to-indigo-900\/50 {
-  --tw-gradient-to: color-mix(in oklab, var(--color-indigo-900)
-      /* oklch(0.359 0.144 278.697) = #312c85 */
-      50%, transparent);
-  --tw-gradient-stops: var(--tw-gradient-via-stops, var(--tw-gradient-position), var(--tw-gradient-from) var(--tw-gradient-from-position), var(--tw-gradient-to) var(--tw-gradient-to-position));
+       --tw-gradient-to: color-mix(in oklab, var(--color-indigo-900) /* oklch(0.359 0.144 278.697) = #312c85 */ 50%, transparent);
+       --tw-gradient-stops: var(
+              --tw-gradient-via-stops,
+              var(--tw-gradient-position),
+              var(--tw-gradient-from) var(--tw-gradient-from-position),
+              var(--tw-gradient-to) var(--tw-gradient-to-position)
+       );
 }
 
 @property --tw-gradient-position {
-  syntax: "*";
-  inherits: false;
+       syntax: "*";
+       inherits: false;
 }
 
 @property --tw-gradient-from {
-  syntax: "<color>";
-  inherits: false;
-  initial-value: #0000;
+       syntax: "<color>";
+       inherits: false;
+       initial-value: #0000;
 }
 
 @property --tw-gradient-via {
-  syntax: "<color>";
-  inherits: false;
-  initial-value: #0000;
+       syntax: "<color>";
+       inherits: false;
+       initial-value: #0000;
 }
 
 @property --tw-gradient-to {
-  syntax: "<color>";
-  inherits: false;
-  initial-value: #0000;
+       syntax: "<color>";
+       inherits: false;
+       initial-value: #0000;
 }
 
 @property --tw-gradient-stops {
-  syntax: "*";
-  inherits: false;
+       syntax: "*";
+       inherits: false;
 }
 
 @property --tw-gradient-via-stops {
-  syntax: "*";
-  inherits: false;
+       syntax: "*";
+       inherits: false;
 }
 
 @property --tw-gradient-from-position {
-  syntax: "<length-percentage>";
-  inherits: false;
-  initial-value: 0%;
+       syntax: "<length-percentage>";
+       inherits: false;
+       initial-value: 0%;
 }
 
 @property --tw-gradient-via-position {
-  syntax: "<length-percentage>";
-  inherits: false;
-  initial-value: 50%;
+       syntax: "<length-percentage>";
+       inherits: false;
+       initial-value: 50%;
 }
 
 @property --tw-gradient-to-position {
-  syntax: "<length-percentage>";
-  inherits: false;
-  initial-value: 100%;
+       syntax: "<length-percentage>";
+       inherits: false;
+       initial-value: 100%;
 }
 </style>
