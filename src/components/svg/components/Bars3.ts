ad75ee2c8c43f66b37785882cf629c7304bf067b
zhouzhijun <15642532452@163.com>
2025-04-29 16:43:35 +0800

    feat(svg): 新增 UserCircle 图标并统一代码格式
    新增 UserCircle 图标组件，并在 index.ts 中导出。同时，将 Bars3.ts 中的单引号改为双引号，以统一代码格式。

--- src/components/svg/components/Bars3.ts
+++ src/components/svg/components/Bars3.ts
@@ -3,8 +3,8 @@ import { defineIcon, type IconProps } from "../script/public";
 
 // 用户图标
 const Bars3: FunctionalComponent<IconProps> = defineIcon({
-       'stroke-linecap': 'round',
-       'stroke-linejoin': 'round',
-       d: 'M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-})
-export { Bars3 as Bars3Icon }
+       "stroke-linecap": "round",
+       "stroke-linejoin": "round",
+       d: "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+});
+export { Bars3 as Bars3Icon };
