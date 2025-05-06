67153de53db955bc9e48b0668dfe79d51a4ee735
zhouzhijun <15642532452@163.com>
2025-05-06 09:08:18 +0800

    refactor(TableShow): 移除不必要的仪表盘组件及样式
    清理了TableShow.vue中与仪表盘相关的冗余代码，简化了页面结构，使其更专注于表格展示功能。

--- src/pages/TableShow.vue
+++ src/pages/TableShow.vue
@@ -1,29 +1,11 @@
 <template>
-       <div class="Dashboard w-full h-full overflow-x-hidden overflow-y">
-              <h1 class="text-3xl mb-5 text-base-content select-none">欢迎来到仪表盘页面！</h1>
-              <div class="w-full grid lg:grid-cols-3 sm:grid-cols-1 gap-4 overflow-x-hidden *:h-92">
-                     <BarEcharts></BarEcharts>
-                     <LineEcharts></LineEcharts>
-                     <BarEcharts></BarEcharts>
-                     <PieEcharts></PieEcharts>
-                     <CircularEcharts></CircularEcharts>
-                     <RoseEcharts></RoseEcharts>
-              </div>
-       </div>
+       <div class="TableShow w-full h-full overflow-x-hidden overflow-y"></div>
 </template>
-<script lang="ts" setup>
-import BarEcharts from "./components/dashboard/BarEcharts.vue";
-import LineEcharts from "./components/dashboard/LineEcharts.vue";
-import PieEcharts from "./components/dashboard/PieEcharts.vue";
-import CircularEcharts from "./components/dashboard/CircularEcharts.vue";
-import RoseEcharts from "./components/dashboard/RoseEcharts.vue";
-</script>
+<script lang="ts" setup></script>
 <style scoped lang="scss">
-.Dashboard {
+.TableShow {
        // border: 1px solid red;
        box-sizing: border-box;
-       color: #333;
-       text-align: center;
        margin-top: 20px;
 }
 </style>
