67caed9f60a7911f46c452edbb060ca7199b3dcd
zhouzhijun <15642532452@163.com>
2025-05-12 14:20:16 +0800

    refactor(src): 更新类型声明以支持 SVG 模块
    在 vite-env.d.ts 文件中添加了对 '@svg' 模块的类型声明，以支持在 Vue 项目中使用 SVG 组件。这将有助于提高项目的类型安全性，并减少开发时的错误。

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,8 @@
 declare module '*.vue' {
        import { Component } from 'vue'; const component: Component; export default component;
 }
+declare module '@svg' {
+       import { DefineComponent } from 'vue';
+       const component: DefineComponent<{}, {}, any>;
+       export default component;
+     }
